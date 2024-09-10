c926b2b4c704ee958fe233f491cbc3e5d3503dad
Fabio Gianzanti <pardal@hotmail.com>
2024-09-10 15:55:26 -0300

    feat: date UTC

--- src/visitor/mod.rs
+++ src/visitor/mod.rs
@@ -1,3 +1,4 @@
+use chrono::Utc;
 use serde::{Deserialize, Serialize};
 use sqlx::FromRow;
 
@@ -17,8 +18,8 @@ pub struct Visitor {
     pub email: String,
     pub lgpd: bool,
     pub image_rights: bool,
-    pub created_at: chrono::NaiveDateTime,
-    pub updated_at: chrono::NaiveDateTime,
+    pub created_at: chrono::DateTime<Utc>,
+    pub updated_at: chrono::DateTime<Utc>,
     pub observations: String,
     pub confirm_visit: bool,
     pub state: String,
